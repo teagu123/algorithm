e0fa95cbce87b258f0e50553288e9454a07ac3d6
DevTeddy123 <ab315601@naver.com>
2024-03-14 15:03:59 +0900

    study : Part3

---
+++ Part3/3. 숫자만 출력.js
@@ -0,0 +1,5 @@
+function solution(str) {
+	return Number(str.replace(/[^0-9]/g, ''))
+}
+let str = 'g0en2T0s8eSoft'
+console.log(solution(str))
---
+++ Part3/5. 문자열 압축.js
@@ -0,0 +1,4 @@
+function solution() {
+	return 'test'
+}
+console.log(solution())
