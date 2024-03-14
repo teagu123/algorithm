de44f74c5a0dce5f84dcaaf61f4ee32fe5548231
DevTeddy123 <ab315601@naver.com>
2024-03-14 15:27:10 +0900

    study : part3

--- Part3/5. 문자열 압축.js
+++ Part3/5. 문자열 압축.js
@@ -1,4 +1,37 @@
-function solution() {
-	return 'test'
+function solution(s) {
+	let answer = ''
+	let cnt = 1
+	s = s + ' '
+	for (let i = 0; i < s.length - 1; i++) {
+		if (s[i] === s[i + 1]) cnt++
+		else {
+			answer += s[i]
+			if (cnt > 1) answer += String(cnt)
+			cnt = 1
+		}
+	}
+	return answer
 }
-console.log(solution())
+console.log(solution('KKHSSSSSSSE'))
+
+//가독성이 아래가 더 좋지않다
+function solution(s) {
+	let cnt = 1
+	let result = ''
+
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === s[i + 1]) {
+			cnt++
+		} else {
+			if (cnt === 1) {
+				result += s[i]
+			} else {
+				result += s[i]
+				result += String(cnt)
+				cnt = 1
+			}
+		}
+	}
+	return result
+}
+console.log(solution('KKHSSSSSSSE'))
