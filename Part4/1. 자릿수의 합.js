f2f113a72eee2906eec8934ae8a7350ee4d2a4aa
DevTeddy123 <ab315601@naver.com>
2024-03-15 23:22:47 +0900

    study : part4

--- Part4/1. 자릿수의 합.js
+++ Part4/1. 자릿수의 합.js
@@ -1,7 +1,11 @@
 function solution(point, arr) {
 	let first = 0
 	let second = 0
-	for (let i = 0; i < arr.length; i++) {}
+	for (let x of arr) {
+		for (let y of x) {
+			console.log(y)
+		}
+	}
 }
 let arr = [128, 460, 603, 40, 521, 137, 123]
 console.log(solution(7, arr))
