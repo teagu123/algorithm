765e7bfc99c240fe65d99e7f83a602d0b86a0776
DevTeddy123 <ab315601@naver.com>
2024-05-02 07:55:04 +0900

    Remove : 폴더 삭제

--- Part3/2. 유효한 팰린드롬.js
+++
@@ -1,9 +0,0 @@
-function solution(str) {
-	//문자열이외는 제거하면 되는것이다
-	let toUpperStr = str.toUpperCase().replace(/[^a-z]/g, '')
-
-	if (toUpperStr.split('').reverse().join('') === toUpperStr) return 'YES'
-	return 'NO'
-}
-let str = 'found7, time: study; Yduts; emit, 7Dnuof'
-console.log(solution(str))
