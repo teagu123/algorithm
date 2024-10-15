deac4e5a2f8b7f45ed9a6f578918adae0d516113
DevTeddy123 <ab315601@naver.com>
2024-10-15 17:24:12 +0900

    level0

--- Level.0/가장큰수찾기.js
+++ Level.0/가장큰수찾기.js
@@ -7,3 +7,12 @@ function solution(array) {
 	return [maxValue, array.indexOf(maxValue)]
 }
 solution([1, 8, 3])
+function solution(array) {
+	/**
+	 * Math.max는 배열을 받지않음 그래서 스프레드 연산자를 통해 arr을 풀고
+	 * 그 값을 indexOf로 찾아서 배열로 출력하는 로직
+	 */
+	let maxValue = Math.max(...array)
+	return [maxValue, array.indexOf(maxValue)]
+}
+solution([1, 8, 3])
