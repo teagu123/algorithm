e2d8e3c51390a471411fd10ef3f310ebcefa5d91
DevTeddy123 <ab315601@naver.com>
2025-02-06 09:05:25 +0900

    다시 풀이

--- 프로그래머스 100문제/신고 결과 받기.js
+++ 프로그래머스 100문제/신고 결과 받기.js
@@ -41,3 +41,37 @@ console.log(
 		2,
 	),
 )
+
+// 다시 풀이
+function solution(id_list, report, k) {
+	const arr = [...new Set(report)].map(el => el.split(' '))
+
+	const countList = {}
+	const dataList = {}
+	const answer = []
+
+	for (let el of arr) {
+		const [fir, sec] = el
+		countList[sec] ? (countList[sec] += 1) : (countList[sec] = 1)
+		dataList[fir] ? dataList[fir].push(sec) : (dataList[fir] = [sec])
+	}
+
+	for (let i of id_list) {
+		let count = 0
+
+		if (!dataList[i]) {
+			answer.push(count)
+			count = 0
+			continue
+		}
+
+		for (let j of dataList[i]) {
+			if (countList[j] >= k) {
+				count += 1
+			}
+		}
+		answer.push(count)
+	}
+
+	return answer
+}
