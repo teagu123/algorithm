e955e63ec0dc3e57a16ed6bbba5dd9f4dd5a01b7
DevTeddy123 <ab315601@naver.com>
2025-02-19 23:05:22 +0900

    섹션 6 풀이

--- section6/공주 구하기.js
+++ section6/공주 구하기.js
@@ -7,3 +7,22 @@ function solution(a, b) {
 	}
 	return arr[0]
 }
+
+function solution(n, k) {
+	const mapArr = Array.from({ length: n }, () => 0).map((e, idx) => idx + 1)
+
+	let count = 1
+	while (mapArr.length !== 1) {
+		if (count === 3) {
+			mapArr.shift()
+			count = 1
+			continue
+		}
+
+		count++
+		const fir = mapArr.shift()
+		mapArr.push(fir)
+	}
+
+	return mapArr
+}
