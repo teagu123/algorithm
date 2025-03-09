292ce81e1293c1d98efd3fee14b3186d26e8dc30
DevTeddy123 <ab315601@naver.com>
2025-03-09 18:48:55 +0900

    level01

---
+++ Level.1/체육복.js
@@ -0,0 +1,15 @@
+function solution(n, lost, reserve) {
+	let lost_student = lost.filter(v => !reserve.includes(v)).sort()
+	let count = n - lost_student.length
+	let reserve_student = reserve.filter(v => !lost.includes(v)).sort()
+
+	for (let i = 0; i < reserve_student.length; i++) {
+		if (lost_student.includes(reserve_student[i] - 1)) {
+			lost_student = lost_student.filter(v => v !== reserve_student[i] - 1)
+		} else if (lost_student.includes(reserve_student[i] + 1)) {
+			lost_student = lost_student.filter(v => v !== reserve_student[i] + 1)
+		}
+	}
+
+	return n - lost_student.length
+}
