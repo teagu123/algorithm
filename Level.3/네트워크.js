8514bb648d37f845c6d3e2b27a33ef4a181e792f
DevTeddy123 <ab315601@naver.com>
2025-03-24 18:07:04 +0900

    dfs level 3

---
+++ Level.3/네트워크.js
@@ -0,0 +1,22 @@
+function solution(n, computers) {
+	const visit = Array.from({ length: n }, () => 0)
+	let cnt = 0
+
+	const dfs = level => {
+		visit[level] = 1
+		for (let i = 0; i < n; i++) {
+			if (computers[level][i] === 1 && visit[i] === 0) {
+				dfs(i)
+			}
+		}
+	}
+
+	for (let i = 0; i < n; i++) {
+		if (visit[i] === 0) {
+			dfs(i)
+			cnt++
+		}
+	}
+
+	return cnt
+}
